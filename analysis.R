bb0697812c4ee6bc35a7520857731ad9caf5a56d
Sergey HPC <ovcharenko.ser@gmail.com>
2014-10-27 23:48:01 +0300

    Add cross-validation function and RMSE function.

--- analysis.R
+++ analysis.R
@@ -1,9 +1,17 @@
+require(mgcv)
+require(boot)
+
 dfr <- read.csv('data//TrainingDataset.csv')
 
 rmsle <- function(y, y_hat){
   # calculate the root mean square logarithmic error
   # see: http://www.kaggle.com/c/online-sales/details/Evaluation
   return(sqrt(sum((log(y_hat+1) - log(y+1))^2)/length(y)))
+}
+
+rmse <- function(y, y_hat){
+  # calculate the root mean square error
+  return(sqrt(mean((y_hat - y)^2)))
 }
 
 quantitativeFeatures <- function(dfr){
@@ -56,4 +64,44 @@ imputeMissing <- function(dfr){
   dfr[is.na(dfr[,'Quan_27']),'Quan_27'] <- median(dfr[,'Quan_27'], na.rm=T)
   dfr[is.na(dfr[,'Quan_28']),'Quan_28'] <- median(dfr[,'Quan_28'], na.rm=T)
   return(dfr)
+}
+
+cv <- function(train_fun, predict_fun, data, response_col, K=5, cost=rmse){
+  # Performs a K-fold cross-validation
+  # Args:
+  #    train_fun: a function used to train a model. Must accept a single agrument
+  #      with training data, and return the trained model
+  #    predict_fun: a function used for prediction. Must accept two arguments:
+  #      first is a trained model, second is the test fold. Must return predicted
+  #      output for the test data
+  #    data: a data.frame to split into train/test sets
+  #    response_col: response column name
+  #    K: number of folds, default = 5
+  #    cost: a cost function. The first argument is the true values, the second is
+  #      the predicted values
+  #
+  # Value:
+  #    returns an averaged output of the cost function across folds.
+  # Example:
+  #    cv(function(x) glm(formula = Outcome ~ ., data = x), predict, data, 'Outcome',K=10)
+
+  # prepare errors metric
+  err <- rep(NA, K)
+  # randomly shuffle the data
+  data <- data[sample(nrow(data)),]
+  # Create K equally size folds
+  folds <- cut(seq(1,nrow(data)),breaks=K,labels=FALSE)
+  #Perform K fold cross validation
+  for(i in 1:K){
+    #Segement data by fold
+    testIndexes <- which(folds==i,arr.ind=TRUE)
+    testData <- data[testIndexes, ]
+    trainData <- data[-testIndexes, ]
+    # train model
+    model <- train_fun(trainData)
+    # predict test set
+    prediction <- predict_fun(model, testData)
+    err[i] <- cost(testData[,response_col], prediction)
+  }
+  mean(err)
 }
